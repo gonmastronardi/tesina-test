33c7c6399313da235e86a842dd5318fe54f5364f
FaacuuDiaz <facundodiazgira@gmail.com>
2023-10-20 22:49:41 -0300

    Comparator final

--- comparator/src/classes/fieldStrategies/VowelsCalculator.js
+++ comparator/src/classes/fieldStrategies/VowelsCalculator.js
@@ -7,25 +7,18 @@ export default class CharSimilarityCalculator extends SimpleObjectSimilarityCalc
   }
 
 
-  async GetNumberWords(aString){
-    // console.log(`palabra ${aString}`)
-    // console.log(aString.split(" "))
+  GetNumberWords(aString){
     const count = {}
     for(var word of aString.split(" ")){
-      // console.log("word: ", word)
-      // console.log("count: ", count)
       const parseWord = word.toLowerCase()
       if(Object.keys(count).includes(parseWord)) 
         count[parseWord]++;
       else
         count[parseWord] = 1
     }
-    // console.log(`count ${Object.entries(count)}`)
     const resul  = Object.entries(count).map(([k,v])=> [k,1/v])
-    // console.log(`resul: ${resul}`)
     const response = resul.map(([k,v])=>v).reduce((prev,curr)=> parseFloat(prev)+parseFloat(curr),0);
-    // console.log(`resultado :  ${response}`)
-    return Promise.resolve(response)
+    return response
   }
 
 
@@ -35,17 +28,9 @@ export default class CharSimilarityCalculator extends SimpleObjectSimilarityCalc
   async getSimilarity(aString, anotherString) {
     if (((aString&&anotherString)!== undefined)){
       //do stuff if query is defined and not null
-      this.GetNumberWords(aString)
-      .then((data_A)=>{
-        this.GetNumberWords(anotherString)
-        .then((data_B)=>{
-          // console.log(`a: ${data_A}`)
-          // console.log(`b: ${data_B}`)
-          return Promise.resolve(data_A / data_B)
-        })
-        .catch((e)=>console.log('error b'))
-      })
-      .catch((e)=>console.log('error a'))
+      const data_A = this.GetNumberWords(aString)
+      const data_B = this.GetNumberWords(anotherString)
+      return data_A / data_B
     } else {
       return 0
     }
